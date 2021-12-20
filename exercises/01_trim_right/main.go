15e62f0b13237bca6f26efc1ddb0b7f71e7a2c31
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-12-20 02:14:39 +0200

    01_trim_right

---
+++ exercises/01_trim_right/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func InSet(b byte, set []byte) bool {
+	for _, v := range set {
+		if v == b {
+			return true
+		}
+	}
+	return false
+}
+
+var trimSet = []byte(" \r\n\t")
+
+func TrimLeft(s string) string {
+	for s != "" && InSet(s[len(s)-1], trimSet) {
+		s = s[:len(s)-1]
+	}
+	return s
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("%q\n", TrimLeft(text))
+}
